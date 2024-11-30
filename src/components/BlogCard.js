47ecbc59a42714a1ee296fb83ea9b39ff8781af1
Sadia Afrin <afrinzzxx@gmail.com>
2024-11-30 21:40:34 +0600

    Section-3,marquee,feature,blog of body is completed

---
+++ src/components/BlogCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const BlogCard = () => {
+  return (
+    <div className='col-3'>
+        <div className='blog-card'>
+            <div className='card-image '>
+                <img src='/images/blog-1.jpg' className='img-fluid' alt='blog'/>
+            </div>
+            <div className='blog-content'>
+                <p className='date'>30 Nov, 2024</p>
+                <h5 className='title'>A beautiful Sunday morning renaissance</h5>
+                <p className='desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aspernatur reprehenderit ipsam laudantium nobis voluptates alias doloribus tenetur esse facilis? Eum sapiente illum error saepe, cupiditate tempora velit inventore necessitatibus!</p>
+                <Link to='/' className='button'>Read More</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BlogCard
