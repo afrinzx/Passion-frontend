5ef0b2734f1b78694b5e69bd0d37cda162c6bda6
Sadia Afrin <afrinzzxx@gmail.com>
2024-12-01 21:01:43 +0600

    Filter of Our Store is completed

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const BreadCrumb = () => {
+const BreadCrumb = (props) => {
+  const { title } = props;
   return (
-    <div className='breadcrumb' py-4>
-        <div className='container-xxl'>
-            <div className='row'>
-                <div className='col=12'>
-                    {/* 3:40:00 */}
-                </div>
-            </div>
+    <div className="breadcrumb mb-0 py-4">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col=12">
+            <p className="text-center mb-0 ">
+              <Link to="/" className="text-dark">
+                Home &nbsp;
+              </Link>
+              / {title}
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BreadCrumb
+export default BreadCrumb;
